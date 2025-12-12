1c1c8c6a345e267022a867c3ee93e9380799c476
Евгений <bar_maleyka@list.ru>
2025-12-12 16:28:45 +0300

    Initial commit

---
+++ src/pages/AccountSettingsPage/AccountSettingsPage.tsx
@@ -0,0 +1,5 @@
+const AccountSettingsPage: React.FC = () => {
+  return <h1>Account settings</h1>
+}
+
+export default AccountSettingsPage
---
+++ src/pages/ProductDetailsPage/AccountDetailsPage.tsx
@@ -0,0 +1,13 @@
+import {Helmet} from 'react-helmet'
+
+
+const AccountDetailsPage: React.FC = () => {
+  return <>
+  <Helmet>
+    <title> Cтраница продукта - Matketplace HorseSmart</title>
+  </Helmet>
+  <h1> Страница продукта</h1>
+  </>
+}
+
+export default AccountDetailsPage
