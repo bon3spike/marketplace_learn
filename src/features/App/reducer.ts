665c2808557e4d8d24bdac80e3de984a52efad80
Евгений <bar_maleyka@list.ru>
2025-12-12 23:04:51 +0300

    v1.2.1

---
+++ src/features/App/reducer.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { T_AppThunk, T_Dispatch } from 'store/types'
+import { I_AppStore } from './types'
+
+const initialState: I_AppStore = {
+  isLogged: false,
+  isAppLoading: false,
+}
+
+const appSlice = createSlice({
+  name: 'APP',
+  initialState,
+  reducers: {
+    setIsLogged(state: I_AppStore, action: PayloadAction<boolean>) {
+      state.isLogged = action.payload
+    },
+    setIsAppLoading(state: I_AppStore, action: PayloadAction<boolean>) {
+      state.isAppLoading = action.payload
+    },
+  },
+})
+
+export const { setIsLogged: setIsLoggedAction, setIsAppLoading: setIsAppLoadingAction } =
+  appSlice.actions
+
+export const setIsLogged =
+  (isLogged: boolean): T_AppThunk =>
+  (dispatch: T_Dispatch) => {
+    dispatch(setIsLoggedAction(isLogged))
+  }
+
+export const setIsAppLoading =
+  (isAppLoading: boolean): T_AppThunk =>
+  (dispatch: T_Dispatch) => {
+    dispatch(setIsAppLoadingAction(isAppLoading))
+  }
+
+export default appSlice.reducer
---
+++ src/store/reducers.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from '@reduxjs/toolkit'
+
+import app from 'features/App/reducer'
+
+export default combineReducers({
+    app,
+    //userData,
+    //cart,
+    //favorites
+})
